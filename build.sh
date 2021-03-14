710e551ff4f6f8b670a1ef720e0f6118a7369e3b
Leandro Marques <leandromqrs@hotmail.com>
2021-03-14 16:42:01 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,4 +14,16 @@ work_dir="${base_path}/work"
 out_dir="${base_path}/out"
 
 # mkarchiso -v -w /path/to/work_dir -o /path/to/out_dir /path/to/profile/
-mkarchiso -v -w "${work_dir}" -o "${out_dir}" "${base_path}/main"
+
+
+build_iso() {
+    # Check if work_dir exists and delete then
+    if [ -d "${work_dir}" ]; then
+        rm -rf "${work_dir}"
+    fi
+    mkarchiso -v -w "${work_dir}" -o "${out_dir}" "${base_path}/main"
+}
+
+build_iso
+
+exit 0
