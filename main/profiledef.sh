8fcce1a664007888cb1618db70655731109a7c8a
Leandro Marques <leandromqrs@hotmail.com>
2021-04-04 12:06:33 -0300

    user: fix user live login

--- main/profiledef.sh
+++ main/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="luminos-baseline"
+iso_name="luminos-main"
 iso_label="LUMINOS_$(date +%Y%m)"
 iso_publisher="LuminOS Linux <https://www.archlinux.org>"
-iso_application="LuminOS Linux baseline"
+iso_application="LuminOS Linux main"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
@@ -12,4 +12,5 @@ arch="x86_64"
 pacman_conf="pacman.conf"
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:0400"
 )
