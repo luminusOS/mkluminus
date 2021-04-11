adc7f54a8f5108b76e6ea1e7b4990a2aeed0ecd7
Leandro Marques <leandromqrs@hotmail.com>
2021-04-11 14:01:30 -0300

    update: build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 #
-# Leandro Marques
-# Email  : leandromqrs@hotmail.com
+# SPDX-License-Identifier: GPL-3.0-or-later
 #
 # build.sh
 #
 # The main script that runs the build
 #
 
+
+#TODO:
+#    - Make build.sh accept arguments.
+#    - Optionally use /tmp folder for work dir
+#    - Optionally use UEFI bot in run_archiso QEMU
+
 # Internal config
 base_path="$( cd "$( dirname "$0" )" && pwd )"
-work_dir="/tmp/archiso-tmp"#"${base_path}/work"
+work_dir="/tmp/archiso-tmp"
+#"${base_path}/work"
 out_dir="${base_path}/out"
 date_today="$( date +'%Y.%m.%d' )"
 
@@ -19,7 +25,7 @@ date_today="$( date +'%Y.%m.%d' )"
 # chmod -R 755 work/
 # ln -sfn /usr/lib/systemd/system/sddm.service display-manager.service
 
-config_iso() {
+_config_iso() {
     echo "[makeiso] Checking dependencies..."
     if [ ! -f "/usr/bin/mkarchiso" ]; then
         echo "[makeiso] ERROR: package 'archiso' not found."
@@ -27,23 +33,24 @@ config_iso() {
     fi
 }
 
-build_iso() {
+_build_iso() {
     # Check if work_dir exists and delete then
     # Necessary for rebuild the iso with base configurations if have any changes.
     # See https://wiki.archlinux.org/index.php/Archiso#Removal_of_work_directory
     if [ -d "${work_dir}" ]; then
         echo "[makeiso] Deleting work folder..."
-        echo "[makeiso] Succesfully deleted '$(rm -rfv "${work_dir}" | wc -l)' files"
+        echo "[makeiso] Succesfully deleted $(rm -rfv "${work_dir}" | wc -l) files"
     fi
     exec mkarchiso -v -w "${work_dir}" -o "${out_dir}" "${base_path}/base"
 }
 
-run_iso() {
+_run_iso() {
+    echo "AAAAAA"
     run_archiso -u -i "${base_path}/out/luminos-main-${date_today}-x86_64.iso"
 }
 
-config_iso
-build_iso
-run_iso
+_config_iso
+_build_iso
+_run_iso
 
 exit 0
